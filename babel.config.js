219c716909e2e510d761b28248c5c1784c1f43cb
HajiAli <92437693+AliSinaYOusofi@users.noreply.github.com>
2026-03-06 11:51:45 +0430

    no bugs for now

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
     plugins: [
-      "nativewind/babel",
-      "react-native-reanimated/plugin" 
+      'react-native-worklets/plugin',  // Must be last
     ],
   };
 };
